a9c3c9e8df6225c51f5c8860f22b6f39f8e62d3e
Max Veasey <mveasey87@gmail.com>
2017-11-12 20:03:10 +0000

    script now scrapes content for all internal links of base url provided

--- prototype.py
+++ prototype.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3.6
 
-import sys, requests, html5lib
+import sys, requests, html5lib, re
 from bs4 import BeautifulSoup
 from docx import Document
 
@@ -11,63 +11,82 @@ if len(sys.argv) != 3:
 else:
     filename = sys.argv[1]
     url = sys.argv[2]
-    
+    path = url.split('.com')[1]
+    authority = url.split(path)[0]
+    document = Document()
+
     # Parse website URL provided
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html5lib')
 
-    # Create document and add heading
-    document = Document()
-    document.add_heading(url, 0)
+    # Find all webpages for this site we want to content scrape
+    internal_urls = []
+    links = soup.find_all('a')
+    for link in links:
+        if link.has_attr('href'):
+            href = link['href']
+            if re.match(path, href):
+                internal_urls.append(authority + href)
 
-    # Find page meta name and meta description
-    # TODO Is there a more performant or succinct way
-    metas = soup.find_all('meta')
+    unique_urls = []
+    for url in internal_urls:
+        if url not in unique_urls:
+            unique_urls.append(url)
 
-    # TODO Add ':' after title and description
-    for meta in metas:
-        if 'name' in meta.attrs and meta.attrs['name'] == 'title':
-                name = document.add_paragraph()
-                name.add_run(meta.attrs['name']).bold = True
-                document.add_paragraph(meta.attrs['content'])
-                break
+    # TODO add comment
+    for url in unique_urls:
+        # Parse website URL provided
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'html5lib')
+        document.add_heading(url, 0)
 
-    for meta in metas:
-        if 'name' in meta.attrs and meta.attrs['name'] == 'description':
-                description = document.add_paragraph()
-                description.add_run(meta.attrs['name']).bold = True
-                document.add_paragraph(meta.attrs['content'])
-                break
+        # Find page meta name and meta description
+        # TODO Is there a more performant or succinct way
+        metas = soup.find_all('meta')
+        
+        document.add_paragraph().add_run('Title:').bold = True
+        for meta in metas:
+            if 'name' in meta.attrs and meta.attrs['name'] == 'title':
+                    document.add_paragraph(meta.attrs['content'])
+                    break
 
-    # TODO add 'Content:'' text here
+        document.add_paragraph().add_run('Description:').bold = True
+        for meta in metas:
+            if 'name' in meta.attrs and meta.attrs['name'] == 'description':
+                    document.add_paragraph(meta.attrs['content'])
+                    break
 
-    # Stipulate all the body text we are interestd in 
-    wanted_tags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'li']
+        document.add_paragraph().add_run('Content:').bold = True
 
-    # Find all divs that contain paragraph text
-    p_divs = []
-    p_tags = soup.find_all('p')
-    for p in p_tags:
-        p_divs.append(p.parent)
+        # Stipulate all the body text we are interestd in 
+        wanted_tags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'li']
 
-    # Add all info from the div to the document
-    for div in p_divs:
-        for tag in div.descendants:
-            if tag.name in wanted_tags:
-                # If header tag add heading and type
-                if tag.name[0] == 'h':
-                    header = f'{tag.text} - ({tag.name})'
-                    document.add_heading(header, level=1)
-                # If ordered list item add number
-                # TODO will this increment number
-                elif tag.name == 'li' and tag.parent.name == 'ol':
-                    document.add_paragraph(tag.text, style='List Number')
-                # If unordered list item add bullet point
-                elif tag.name == 'li' and tag.parent.name == 'ul':
-                    document.add_paragraph(tag.text, style='List Bullet')
-                # Else paragraph, no formatting needed
-                else:
-                    document.add_paragraph(tag.text)
+        # Find all divs that contain paragraph text
+        p_divs = []
+        p_tags = soup.find_all('p')
+        for p in p_tags:
+            p_divs.append(p.parent)
 
+        # Add all info from the div to the document
+        for div in p_divs:
+            for tag in div.descendants:
+                if tag.name in wanted_tags:
+                    # If header tag add heading and type
+                    if tag.name[0] == 'h':
+                        header = f'{tag.text} - ({tag.name})'
+                        document.add_heading(header, level=1)
+                    # If ordered list item add number
+                    # TODO will this increment number
+                    elif tag.name == 'li' and tag.parent.name == 'ol':
+                        document.add_paragraph(tag.text, style='List Number')
+                    # If unordered list item add bullet point
+                    elif tag.name == 'li' and tag.parent.name == 'ul':
+                        document.add_paragraph(tag.text, style='List Bullet')
+                    # Else paragraph, no formatting needed
+                    else:
+                        document.add_paragraph(tag.text)
+
+
+        document.add_page_break()
 
     document.save(filename)
--- test.py
+++ test.py
@@ -1,77 +1,34 @@
 #!/usr/bin/env python3.6
 
-import sys, requests
+import sys, requests, re
 from bs4 import BeautifulSoup
 from docx import Document
 
 # Parse website URL provided
-r = requests.get('https://www.movenpick.com/en/asia/philippines/boracay/resort-spa-boracay/wellness/')
+url = 'https://www.movenpick.com/en/asia/philippines/boracay/resort-spa-boracay/'
+r = requests.get(url)
 soup = BeautifulSoup(r.text, 'html.parser')
+path = url.split('.com')[1]
+# print(tree)
+path = url.split('.com')[1]
+authority = url.split(path)[0]
 
-head = soup.head
+# reg = re.compile(r"^{}".format(tree))
 
-body = soup.body
+# print(reg)
 
-ps = soup.find_all('p')
+# Find all webpages for this site we want to content scrape
+internal_links = []
+links = soup.find_all('a')
+for link in links:
+    if link.has_attr('href'):
+        href = link['href']
+        if re.match(path, href) and href not in internal_links:
+            internal_links.append(authority + href)
 
-ul = soup.find_all('ul')
 
-ps = soup.find_all('p')
-
-divs = soup.find_all('div')
-
-#TODO do all internal links start with '/'?
-# a = soup.find_all('a')
-
-# for z in a:
-#     if 'href' in z.attrs:
-#         print(z['href'])
-
-wanted_tags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'li']
-# fetched_tags = soup.find_all(tags)
-
-p_divs = []
-p_tags = soup.find_all('p')
-for p in p_tags:
-    p_divs.append(p.parent)
-
-for div in p_divs:
-    for tag in div.descendants:
-        if tag.name in wanted_tags:
-            # If header tag add heading and type
-            if tag.name[0] == 'h':
-                header = f'{tag.text} - ({tag.name})'
-                print(header)
-            # If ordered list item add number
-            # TODO will this increment number
-            elif tag.name == 'li' and tag.parent.name == 'ol':
-                print(tag.text)
-            # If unordered list item add bullet point
-            elif tag.name == 'li' and tag.parent.name == 'ul':
-                print(tag.text)
-            # Else paragraph, no formatting needed
-            else:
-                print(tag.text)
-#------------------------------------------
-# for div in divs:
-#     print(div.contents)
-#     print()
-
-# p1 = body.find_next('p')
-# p2 = soup.find_next('p')
-# print(p1)
-# print(':')
-
-# # print(p1.find_parent)
-# for p in ps:
-#     print(p.find_next_sibling)
-
-# print(body.prettify())
-# print(dir(ul[0]))
-# print()
-# print(ul[0].find_parent())
-
-# for p in ps:
-#     print(p.next_sibling)
-#     print(p.previous_sibling)
-#     print()
+print(internal_links)
+# new_list = []
+# [new_list.append(item) for item in internal_links if item not in new_list]
+# for link in new_list:
+#     print(link)
